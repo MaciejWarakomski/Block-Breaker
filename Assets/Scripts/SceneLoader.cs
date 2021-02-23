508f2bf3160a2f1941e86e3a9a1b1cb807b0725e
Maciej Warakomski <Warakom247365@gmail.com>
2021-02-23 21:34:45 +0100

    Initial commit

---
+++ Assets/Scripts/SceneLoader.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneLoader : MonoBehaviour
+{
+
+    public void LoadNextScene()
+    {
+        int currentSceneIndex = SceneManager.GetActiveScene().buildIndex;
+        SceneManager.LoadScene(currentSceneIndex + 1);
+        if (currentSceneIndex + 2 == SceneManager.sceneCountInBuildSettings)
+        {
+            Cursor.visible = true;
+        }
+        else
+        {
+            Cursor.visible = false;
+        }
+        
+    }
+
+    public void LoadStartScene()
+    {
+        SceneManager.LoadScene(0);
+        FindObjectOfType<GameSession>().ResetGame();
+        Cursor.visible = true;
+    }
+
+    public void LoadGameOverScene()
+    {
+        SceneManager.LoadScene("Game Over");
+        Cursor.visible = true;
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+}
